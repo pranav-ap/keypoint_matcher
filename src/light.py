3936162168cda479cdd5fbf1cbd689ef350878fd
pranav <pranavap96@gmail.com>
2024-11-19 23:43:52 +0100

    refactor aug

--- src/light.py
+++ src/light.py
@@ -16,7 +16,7 @@ torch.set_float32_matmul_precision('medium')
 
 
 class Light(pl.LightningModule):
-    def __init__(self, neptune_logger, tensorboard_logger):
+    def __init__(self, neptune_logger=None, tensorboard_logger=None):
         super().__init__()
 
         self.neptune_logger = neptune_logger
@@ -173,17 +173,19 @@ class Light(pl.LightningModule):
         out_path = os.path.join(out_path, name)
         image_grid.save(out_path)
 
-        self.neptune_logger.experiment[f"{stage}/images"].append(
-            File.as_image(image_grid),
-            step=self.global_step,
-            name=name,
-        )
+        if self.neptune_logger is not None:
+            self.neptune_logger.experiment[f"{stage}/images"].append(
+                File.as_image(image_grid),
+                step=self.global_step,
+                name=name,
+            )
 
-        self.tensorboard_logger.experiment.add_images(
-            tag=f"{stage}_images",
-            img_tensor=get_tensor_grid(image_grid),
-            global_step=self.global_step
-        )
+        if self.tensorboard_logger is not None:
+            self.tensorboard_logger.experiment.add_images(
+                tag=f"{stage}_images",
+                img_tensor=get_tensor_grid(image_grid),
+                global_step=self.global_step
+            )
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
--- utils/logger.py
+++ utils/logger.py
@@ -16,15 +16,23 @@ class MyLogger:
 
     @staticmethod
     def init_loggers():
-        if MyLogger.neptune_logger is None:
+        if 'neptune' in config.loggers and MyLogger.neptune_logger is None:
             MyLogger.neptune_logger = MyLogger.get_neptune_logger()
 
-        if MyLogger.tensorboard_logger is None:
+        if 'tensorboard' in config.loggers and  MyLogger.tensorboard_logger is None:
             MyLogger.tensorboard_logger = MyLogger.get_tensorboard_logger()
 
     @staticmethod
     def get_loggers():
-        return [MyLogger.neptune_logger, MyLogger.tensorboard_logger]
+        loggers = []
+
+        if MyLogger.neptune_logger is not None:
+            loggers.append(MyLogger.neptune_logger)
+
+        if MyLogger.tensorboard_logger is not None:
+            loggers.append(MyLogger.tensorboard_logger)
+
+        return loggers
 
     @staticmethod
     def init_loguru():
